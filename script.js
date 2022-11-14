48f34fa991d243c58f661e414105b11cf359ddca
Nathaniel Motley <nmotley92@gmail.com>
2022-11-14 15:41:13 -0600

    adds event listener for click on save buttons

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 var currentDayEl= $('#currentDay');
 var currentDay= dayjs().format('dddd, MMMM D' )
+var saveButtonsEl= $('.btn');
 
 // current day
 currentDayEl.text(currentDay);
+// event listener for save buttons
+saveButtonsEl.click(function(){
+  var id = $(this).parent().attr('id');
+  var value = $(this).prev().val();
+  localStorage.setItem(id, value);
+})
 
